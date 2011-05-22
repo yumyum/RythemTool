e384cdce6e3643be5dd93e44c81399d6a5d433cf
yumyum <yumyum7@gmail.com>
2011-05-22 14:49:02 +0900

    多言語対応
    英語リソースを追加

--- src/jp/yumyum/RhythmToolActivity.java
+++ src/jp/yumyum/RhythmToolActivity.java
@@ -3,7 +3,6 @@ package jp.yumyum;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
